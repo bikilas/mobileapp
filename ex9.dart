f53dc92fc1dbce01dc6dc1f8c83e693cf58604f1
Bikila Tariku <=>
2024-03-05 16:58:16 +0300

    car class

---
+++ ex9.dart
@@ -0,0 +1,15 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+}
+
+void main() {
+  Car car = Car('Toyota', 'Camry', 2022);
+
+  print('Brand: ${car.brand}');
+  print('Model: ${car.model}');
+  print('Year: ${car.year}');
+}
