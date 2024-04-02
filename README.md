d2f38fc3e8991798a0642d870b6989f4df6a8993
Bikila Tariku <bikilatariku810@gmail.com>
2024-04-02 17:02:12 +0300

    widget added

--- README.md
+++
@@ -1,3 +0,0 @@
-# mobileapp
-Name:Bikila Tariku
-ID:UGR/8089/14
--- wedgitex1/ex1/android/app/src/main/kotlin/com/example/nana/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nana
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
